412a86a808d3ca2843d1b383ad7d6c11a6abd040
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-11 13:48:34 +0400

    HW Task8, done

---
+++ Homework1_Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Enter the number: ");
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 2; i <= n; i += 2) {
+    Console.Write(i);
+    if (i + 2 <= n)
+        Console.Write(", ");
+}
