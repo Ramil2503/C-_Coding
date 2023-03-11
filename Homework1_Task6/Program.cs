1f360528026452b765c731dbbff2b51e8e4352ae
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-11 13:43:47 +0400

    HW Task6, done

---
+++ Homework1_Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Enter the number: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (n % 2 == 0)
+    Console.Write("The number is even!");
+else
+    Console.Write("The number is odd!");
