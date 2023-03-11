7fbdc1d440d7fd4ec8406c715736f810e1c871d0
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-11 13:19:39 +0400

    HW Task2, done

---
+++ Homework1_Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Enter the first number: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the second number: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b)
+    Console.Write($"max = {a}, min = {b}");
+else
+    Console.Write($"max = {b}, min = {a}");
