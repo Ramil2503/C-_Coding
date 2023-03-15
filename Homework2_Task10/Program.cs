8e9c2c419ab043f265e78ad36daae1854bfbd78f
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-15 14:31:09 +0400

    HW2 Task10, done

---
+++ Homework2_Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Enter number: ");
+int n = int.Parse(Console.ReadLine()!);
+
+while (n > 999 || n < 100) {
+    Console.Write("The number you entered is not 3 digit number!\nEnter correct number: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+
+Console.Write($"{n / 10 % 10}");
