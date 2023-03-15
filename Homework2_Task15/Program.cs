e14a0fafe37fd62e210894f22decf74ca183e1da
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-15 14:46:25 +0400

    HW2 Task15, done

---
+++ Homework2_Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Enter number: ");
+int n = int.Parse(Console.ReadLine()!);
+
+while (n < 1 || n > 7) {
+    Console.Write("Enter correct number: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+
+if (n == 6 || n == 7)
+    Console.Write("yes");
+else
+    Console.Write("no");
