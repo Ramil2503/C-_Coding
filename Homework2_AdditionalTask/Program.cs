7df0c3e7d8547f1794efd600f0ddd51f47a364c0
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-15 19:20:50 +0400

    HW2 AdditionalTask, done

---
+++ Homework2_AdditionalTask/Program.cs
@@ -0,0 +1,33 @@
+﻿//reading input till user will give zero
+//finding second maximum
+Console.Clear();
+
+string input = Console.ReadLine();
+string[] numbers = null;
+
+if (input != null)
+{
+    numbers = input?.Split(' ');
+}
+
+int max1 = 0, max2 = 0;
+
+if (numbers != null)
+{
+    foreach (string number in numbers)
+    {
+        int num = int.Parse(number);
+
+        if (num > max1)
+        {
+            max2 = max1;
+            max1 = num;
+        }
+        else if (num > max2)
+        {
+            max2 = num;
+        }
+    }
+}
+
+Console.WriteLine(max2);
