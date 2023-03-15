591254c588f0e70779b9a52a33a45aa2c13e69a4
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-15 14:42:30 +0400

    HW2 Task13, done

---
+++ Homework2_Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Enter number: ");
+int n = int.Parse(Console.ReadLine()!);
+
+while (n < 100) {
+    Console.Write("There is no 3rd number available!\nEnter correct number: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+
+while (n > 999) {
+    n /= 10;
+}
+
+Console.Write($"{n % 10}");
