7ebf55876d3c8b6465248e6a50a9332ea90db927
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-20 16:22:25 +0400

    HW4 Task29, done

---
+++ Homework4_Task29/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+int[] arr = new int[8];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(1, 101);
+}
+Console.WriteLine("[" + string.Join(", ", arr) + "]");
