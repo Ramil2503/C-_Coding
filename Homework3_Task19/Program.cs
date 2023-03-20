f7f953b9967ab6ca9a98991f2f2263ada9eac45d
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-20 15:25:16 +0400

    HW3 Task19, done

---
+++ Homework3_Task19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+string s = Console.ReadLine()!;
+int j = s.Length - 1, cnt = 0;
+
+for (int i = 0; i < s.Length / 2; i++) {
+    if (s[i] != s[j]) {
+        cnt++;
+        break;
+    }
+    j--;
+}
+
+if (cnt == 0)
+    Console.Write("YES");
+else
+    Console.Write("NO");
