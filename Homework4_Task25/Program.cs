8c3cbac560f2d767a60dc80d5870b1c199c9beac
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-20 16:17:29 +0400

    HW4 Task25, done

---
+++ Homework4_Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+string[] numbers = Console.ReadLine()!.Split();
+
+int A = int.Parse(numbers[0]);
+int B = int.Parse(numbers[1]);
+
+int result = 1;
+for (int i = 0; i < B; i++)
+{
+    result *= A;
+}
+Console.WriteLine(result);
