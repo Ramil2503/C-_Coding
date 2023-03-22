11ba4401f08ae03ac824ccd2e7a78d6dfb82094b
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-22 17:18:32 +0400

    hw3 additional task, error fixed

--- Homework3_AdditionalTask/Program.cs
+++ Homework3_AdditionalTask/Program.cs
@@ -15,7 +15,7 @@ maxSum = arr[0] + arr[1] + arr[2];
 
 for (i = 0; i < n; i++)
 {
-    if (arr[i] != null && arr[i + 1] != null && arr[i + 2] != null)
+    if (arr[i] != 0 && arr[i + 1] != 0 && arr[i + 2] != 0)
     {
         if (arr[i] + arr[i + 1] + arr[i + 2] > maxSum)
             maxSum = arr[i] + arr[i + 1] + arr[i + 2];
