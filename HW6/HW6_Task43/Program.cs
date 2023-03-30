acb76139f7aac925c8c51b7534d7689d55ad6d3f
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-30 12:15:50 +0400

    HW6 Task43, done

---
+++ HW6/HW6_Task43/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter the value for k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the value for b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the value for k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the value for b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения двух прямых: ({x:F1}; {y:F1})");
