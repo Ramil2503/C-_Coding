15768a47e398a0a0779d4b18146eed4735e51ea8
Ramil2503 <ramil.ax2503@gmail.com>
2023-03-31 21:00:58 +0400

    SeminarExamples, done

---
+++ SeminarExamples/Example004_Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] array = new int[3, 4] {
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12}
+};
+
+int numRows = array.GetLength(0);
+int numCols = array.GetLength(1);
+
+Console.WriteLine("Initial Array:");
+for (int i = 0; i < numRows; i++) {
+    for (int j = 0; j < numCols; j++) {
+        Console.Write("{0,2} ", array[i, j]);
+    }
+    Console.WriteLine();
+}
+
+for (int j = 0; j < numCols; j++) {
+    int temp = array[0, j];
+    array[0, j] = array[numRows - 1, j];
+    array[numRows - 1, j] = temp;
+}
+
+Console.WriteLine("\nFinal Array:");
+for (int i = 0; i < numRows; i++) {
+    for (int j = 0; j < numCols; j++) {
+        Console.Write("{0,2} ", array[i, j]);
+    }
+    Console.WriteLine();
+}
---
+++ SeminarExamples/Example007_MinArray/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.RegularExpressions;
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void MinimumInArray(int[,] matrix)
+{
+    int min = 100, column = 0, row = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < min)
+            {
+                min = matrix[i, j];
+                column = i;
+                row = j;
+            }
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i != column && j != row)
+            {
+                Console.Write($"{matrix[i,j]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine()!.Split(" ").Select(x => int.Parse(x)).ToArray();
+// size[0] - row
+// size[1] - column
+int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Начальный массив:");
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine("Конечный массив:");
+MinimumInArray(matrix);
