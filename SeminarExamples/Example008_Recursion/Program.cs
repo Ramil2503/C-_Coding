eeb02b5859842ac6c16f441f35a22b9089639baf
Ramil2503 <ramil.ax2503@gmail.com>
2023-04-07 11:38:35 +0400

    New Seminar task, done

---
+++ SeminarExamples/Example008_Recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿int f(int n)
+{
+    if (n < 10) {
+        return n;
+    }
+    return f(n / 10) + n % 10;
+}
+
+Console.Clear();
+Console.Write("Enter the number: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(n));
+/*
+f(5) -> f(4) + "5 "
+          |
+          f(3) + "4 "
+            |
+            f(2) + "3 "
+              |
+              f(1) + "2 "
+                |
+                "1 "
+*/
